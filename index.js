1bed1fe9d74de880b094efb5bbc42e2a3e518678
prerna2021 <51856236+prerna2021@users.noreply.github.com>
2020-03-19 18:28:32 +0530

    dicee challenge practice

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var randomNumber=Math.floor(Math.random()*6)+1;
+var randomImage="dice"+randomNumber+".png";
+var randomStore="images/" + randomImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomStore);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImage2="dice"+randomNumber2+".png";
+var randomStore2="images/" + randomImage2;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomStore2);
+if(randomNumber>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🔥player1 wins🔥";
+}
+else if(randomNumber<randomNumber2){
+  document.querySelector("h1").innerHTML="🔥player2 wins🔥"
+}
+else
+{
+    document.querySelector("h1").innerHTML="😢Draw😢";
+}
